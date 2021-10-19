e8e6a9ea393a03d4439f44c18548b51ffcaebc30
coke911 <64025072+coke911@users.noreply.github.com>
2021-10-19 16:33:46 +0800

    Add files via upload

---
+++ zz icon.js
@@ -0,0 +1,17 @@
+{
+"name":"ZZ Icon", 
+"description":"Desing By ErdongChan.", 
+"icons":
+[
+	{"name":"5iTV", "url":"https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/5iTV.png"},
+	{"name":"Dler", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Dler.png"},
+	{"name":"Element", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Element.png"},
+	{"name":"Mielink", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Mielink.png"},
+	{"name":"Prpr", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Prpr.png"},
+	{"name":"Hxlm", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Hxlm.png"},
+	{"name":"Skicat", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Skicat.png"},
+	{"name":"Paolu", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Paolu.png"},
+	{"name":"Ssp", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Ssp.png"},
+	{"name":"Imm", "url":"https:\/\/raw.githubusercontent.com\/erdongchanyo\/icon/main\/Airport\/Imm.png"},
+]
+}
